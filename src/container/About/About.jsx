44b1b2af73ba73be91c22c96543641cf15977847
oluku-project <108981922+oluku-project@users.noreply.github.com>
2023-09-26 06:18:00 +0000

    Update About.jsx
    change about head content

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
 	return (
 		<>
 			<h2 className='head-text'>
-				I Know that <span>Good Design</span> <br />
+				We Know that <span>Good Design</span> <br />
 				means <span>Good Business</span>
 			</h2>
 
