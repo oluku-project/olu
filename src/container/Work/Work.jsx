fdfc38ae8f0dc83bac184f250b58561c8d609dfe
oluku-project <108981922+oluku-project@users.noreply.github.com>
2023-09-26 07:20:29 +0000

    Update Work.jsx
    comment git out

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -112,14 +112,14 @@ const Work = () => {
 										<AiFillEye />
 									</motion.div>
 								</a>
-								<a href={work.codeLink} target='_blank' rel='noreferrer'>
+{/* 								<a href={work.codeLink} target='_blank' rel='noreferrer'> */}
 									<motion.div
 										whileInView={{ scale: [0, 1] }}
 										whileHover={{ scale: [1, 0.9] }}
 										transition={{ duration: 0.25 }}
 										className='app__flex'
 									>
-										<AiFillGithub />
+{/* 										<AiFillGithub /> */}
 									</motion.div>
 								</a>
 							</motion.div>
